31c273b31d1624691fe1d734cdc6f0cbd7d93f5a
Edward Chen <chenward.T@gmail.com>
2015-12-22 16:31:08 -0500

    Configure hot module reloading and webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,46 @@ var path = require("path");
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
 
+const INTERFACE = '192.168.1.5';
+
 module.exports = {
   context: __dirname,
 
-  // entry point of our app.
+  // Entry point of our app
   // assets/js/index.js should require other js modules and dependencies it needs
-  entry: './assets/js/index',
+  entry: [
+    'webpack-dev-server/client?http://' + INTERFACE + ':3000',
+    'webpack/hot/only-dev-server',
+    './assets/js/index'
+  ],
 
   output: {
     path: path.resolve('./assets/bundles/'),
-    filename: "[name]-[hash].js"
+    filename: "[name]-[hash].js",
+    // Tell django to use this URL to load packages instead of STATIC_URL + bundle_name
+    publicPath: 'http://' + INTERFACE + ':3000/assets/bundles/'
   },
 
+  devtool: 'source-map',
+
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // Disable reloading if there is an error
+    new webpack.NoErrorsPlugin(),
     new BundleTracker({filename: './webpack-stats.json'})
   ],
 
   module: {
     loaders: [
-      // transform JSX into JS
-      { test: /\.jsx?$/,
+      // Transform JSX into JS
+      // Pass output from babel loader to react-hot loader
+      {
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'es2015',
-            'react'
-          ]
-        }
+        loaders: [
+          'react-hot',
+          'babel?presets[]=react,presets[]=es2015'
+        ]
       }
     ]
   },
