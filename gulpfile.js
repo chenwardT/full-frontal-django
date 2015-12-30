9f9b3a723c47ec1432fceff2b62f67354ce1fa9c
Edward Chen <chenward.T@gmail.com>
2015-12-30 13:25:49 -0500

    Configure gulp task 'django-server' to pipe django server output to console. Change gulpfile to reflect changes to project structure. Cleanup gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-gulp.task('webpack-dev-server', function(callback) {
+gulp.task('webpack-dev-server', function() {
   new WebpackDevServer(webpack(config), {
     // server and middleware options
     publicPath: config.output.publicPath,
@@ -17,24 +17,32 @@ gulp.task('webpack-dev-server', function(callback) {
     if (err) throw new gutil.PluginError('webpack-dev-server', err);
 
     // server listening
-    gutil.log('Listening at localhost:3000')
-  })
+    gutil.log('Listening at localhost:3000');
+  });
 });
 
 // Activate virtualenv (using virtualenvwrapper) and start django-extensions server
 gulp.task('django-server', function() {
   var proc = exec('workon ffd-dev && python manage.py runserver_plus');
+
+  // Pipe django server output to console
+  proc.stderr.on('data', function(data) {
+    process.stdout.write(data);
+  });
+  proc.stdout.on('data', function(data) {
+    process.stdout.write(data);
+  });
 });
 
 gulp.task('browser-sync', function() {
   browserSync.init({
     notify: false,
-    proxy: "127.0.0.1:8001"
+    proxy: '127.0.0.1:8001'
   });
 
+  gulp.watch('base/templates/base/*.html').on('change', browserSync.reload);
   gulp.watch('bucketlist/static/bucketlist/css/*.css').on('change', browserSync.reload);
-  gulp.watch('bucketlist/templates/*.html').on('change', browserSync.reload);
+  gulp.watch('bucketlist/templates/bucketlist/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['webpack-dev-server', 'django-server', 'browser-sync'], function() {
-});
+gulp.task('default', ['webpack-dev-server', 'django-server', 'browser-sync']);
