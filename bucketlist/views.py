9e5aff2d59b913e8a167c66700d060b113f1c3e6
Edward Chen <chenward.T@gmail.com>
2015-12-30 13:19:36 -0500

    Add bucketlist app views: index, detail.

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -4,9 +4,16 @@ from rest_framework import viewsets
 from .serializers import BucketListSerializer
 from .models import BucketList
 
+
 class BucketListViewset(viewsets.ModelViewSet):
     queryset = BucketList.objects.all()
     serializer_class = BucketListSerializer
 
-def test(request):
-    return render(request, 'index.html')
+
+def bucketlist_index(request):
+    bucketlists = BucketList.objects.all()
+    return render(request, 'bucketlist/index.html', context={'bucketlists': bucketlists})
+
+
+def bucketlist_detail(request, id):
+    return render(request, 'bucketlist/detail.html', context={'id': id})
